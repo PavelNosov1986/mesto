221782d0c8808a725660ff90966d2f2d12b562e7
PavelNosov1986 <99183133+PavelNosov1986@users.noreply.github.com>
2022-04-28 16:56:12 +0300

    in progress

--- scripts/srcipt.js
+++ scripts/srcipt.js
@@ -1,27 +1,89 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
+// Открытие и закрытие попапов
 const popup = document.querySelector('.popup');
-const closePopupButton = popup.querySelector('.popup__close');
+const openPopupButtonEdit = document.querySelector('.profile__edit-button');
+const openPopupButtonAddCards = document.querySelector('.profile__add-button');
+const popupEdit = document.getElementById('edit');
+const popupAddcards = document.getElementById('add-cards');
+const closePopupButton = document.getElementById('close-edit');
+const closePopupAddcards = document.getElementById('close-add-cards');
 
-function popupOpenToggle() {
-    popup.classList.toggle('popup_opened');
-    document.getElementById('name').value = document.querySelector('.profile__name').textContent;
-    document.getElementById('description').value = document.querySelector('.profile__about-me').textContent;
+function popupOpen(element) {
+  element.classList.add('popup_opened');
 }
 
-openPopupButton.addEventListener('click', popupOpenToggle);
-closePopupButton.addEventListener('click', popupOpenToggle);
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.getElementById('name');
-let jobInput = document.getElementById('description');
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    let nameInput = document.getElementById('name').value;
-    let jobInput = document.getElementById('description').value;
-    document.querySelector('.profile__name').textContent = nameInput;
-    document.querySelector('.profile__about-me').textContent = jobInput;
-    popupOpenToggle();
+function popupClose(id) {  
+  document.getElementById(id).classList.remove('popup_opened');
 }
 
+function popupOpenToggleEdit() {
+  popupOpen(popupEdit);
+  document.getElementById('name').value = document.querySelector('.profile__name').textContent;
+  document.getElementById('description').value = document.querySelector('.profile__about-me').textContent;
+}
+
+function popupOpenToggleAddCards() {
+  popupOpen(popupAddcards);
+}
+
+// Редактирование попап - edit
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.getElementById('name');
+const jobInput = document.getElementById('description');
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  const nameInput = document.getElementById('name').value;
+  const jobInput = document.getElementById('description').value;
+  document.querySelector('.profile__name').textContent = nameInput;
+  document.querySelector('.profile__about-me').textContent = jobInput;
+  popupClose('edit');
+ }
 formElement.addEventListener('submit', formSubmitHandler);
+
+// Начальные карточки при открытии страницы
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Шаблоны
+
+const cards = document.querySelector('.cards');
+const template = document.querySelector('.template').content;
+
+initialCards.forEach(function (element) {
+  const card = template.cloneNode(true);
+  const img = card.querySelector('.element__image');
+  img.src = element.link;
+  card.querySelector('.element__title').textContent = element.name;
+  cards.appendChild(card);
+});
+
+// Создание новой карточки
+function newCarde() {
+  const firstCarde = cards.cloneNode(true);
+  cards.appendChild(firstCarde);
+
+}
+ newCarde();
